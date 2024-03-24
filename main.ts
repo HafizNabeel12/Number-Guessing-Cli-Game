dcccad7467a6560c4126e68f0c858cd6825f833b
HafizNabeel12 <138764369+HafizNabeel12@users.noreply.github.com>
2024-03-24 13:41:21 +0500

    Created a nuber guessing game

---
+++ main.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env tsm
+
+import inquirer from "inquirer"
+
+
+async function askQuestion() {
+    
+
+
+const number = (Math.floor(Math.random() *10))
+
+
+
+
+const answer = await inquirer.prompt([
+    {
+    "name":"Number_Guessing_Game" ,
+    "type" : "number",
+    "message":"Please Guess a Number",
+    }
+])
+
+if(answer.Number_Guessing_Game==number)
+{
+    console.log("Congratulations!You Won the Game")
+
+}else{
+    console.log("You Guessed Wrong! Please Try Again")
+}
+
+}
+
+async function startAgain() {
+    do{
+        await askQuestion();
+        var again = await inquirer.prompt({
+            type:"input",
+            name:"restart",
+            message:"Do You Want To Continue Press Y or N"
+        })
+    }
+    while(again.restart == "Y" || again.restart == "y" || again.restart == "YES" || again.restart == "yes")
+}
+startAgain();
+
